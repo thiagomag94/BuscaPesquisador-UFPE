a02cb6d3b0a2d04f9d31df77c320c9d7b1c345e7
thiagomag94 <thiagomag94@gmail.com>
2023-02-14 06:37:58 -0300

    changing the request address to backend in env production

--- Front-end/busca-pesquisador/services/RequestAPI/index.tsx
+++ Front-end/busca-pesquisador/services/RequestAPI/index.tsx
@@ -9,7 +9,7 @@ export const RequestAPI = async(listnames:string[]) => {
 
      
     
-     const response =  await axios.post(`http://localhost:3004/professor`, data
+     const response =  await axios.post(`https://backend-sistema-dpq.vercel.app/professor`, data
      )
     console.log(response.data.professores)
      return response.data.professores
