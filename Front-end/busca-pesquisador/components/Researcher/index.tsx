41527fb1251e64e3df9c54ba3c0a0507f7e35f13
thiagomag94 <thiagomag94@gmail.com>
2023-02-25 07:47:26 -0300

    fixing the error in page buscanome type again 2

--- Front-end/busca-pesquisador/components/Researcher/index.tsx
+++ Front-end/busca-pesquisador/components/Researcher/index.tsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { BuscaNomeType } from '../FormSearch/BuscaNomeType';
+
 
 
 
